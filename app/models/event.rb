c8809c2bebc6e3407692c2f335ffb5613910592b
SSapunar <ssapunar@miuandes.cl>
2026-04-30 13:09:34 -0400

    Modelos v1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,21 @@
 class Event < ApplicationRecord
+
   belongs_to :user
   has_many :registrations
   has_many :reviews
   has_many :event_categories
   has_many :categories, through: :event_categories
+  validates :name, :date, :capacity, presence: true
+  validates :capacity, numericality: { greater_than: 0 }
+  enum status: { draft: 0, published: 1, finished: 2 }
+  validate :date_cannot_be_in_the_past
+
+  private
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "cannot be in the past")
+    end
+  end
+  
 end
